b55f4bba82af12e92d1f4843b38c243c9951ea35
Jisoo Lee <angeljess@naver.com>
2019-02-21 17:00:14 +0900

    Day 1 - 2강까지(변수)

--- day1/src/ScannerEx.java
+++ day1/src/ScannerEx.java
@@ -1,15 +1,22 @@
-import java.util.*;
+// ctrl+space : 자동완성
+// alt+up, down : 행이동
+// ctrl+/ : 주석(toggle)
+// ctrl+d : 한줄 삭제
+// ctrl+shift+f : 자동 정렬
+// ctrl+shift+o : 자동 import
+
+import java.util.Scanner;
 
 public class ScannerEx {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("두자리 정수를 하나 입력해주세요.>");
 		String input = scanner.nextLine();
 		int num = Integer.parseInt(input);
-		
-		System.out.println("입력내용 :"+input);
+
+		System.out.println("입력내용 :" + input);
 		System.out.printf("num=%d%n", num);
 		System.out.printf("%n ㅎㅎ");
 	}
