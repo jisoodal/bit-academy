eb1a0b41342fb880ab56cfdee2869ee8350b8546
Jisoo Lee <angeljess@naver.com>
2019-02-21 16:56:12 +0900

    Day1 - 2강까지

---
+++ day1/src/star2.java
@@ -0,0 +1,29 @@
+
+public class star2 {
+
+	public static void main(String[] args) {
+		// 2번 예제
+		
+		/*
+		for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				System.out.print("["+i+","+j+"]");
+			}
+			System.out.println();
+		}
+		*/
+		for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				if(i==j)
+				System.out.print("*");
+				else
+					System.out.print("     ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ day1/src/star3.java
@@ -0,0 +1,33 @@
+
+public class star3 {
+
+	public static void main(String[] args) {
+		// 3번 예제
+		
+		/*
+		for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				if(i+j==4)
+				System.out.print("["+i+","+j+"]");
+				else
+					System.out.print("     ");
+			}
+			System.out.println();
+		}
+		*/
+		
+			for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				if(i+j==4)
+				System.out.print("*");
+				else
+					System.out.print("     ");
+			}
+			System.out.println();
+		}
+			
+	}
+
+}
