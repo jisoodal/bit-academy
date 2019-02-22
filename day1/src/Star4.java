1111cb14be626fce060c86fbbebb74155f824dd5
Jisoo Lee <angeljess@naver.com>
2019-02-22 09:35:33 +0900

    Day2 각 예제를 따로 담기 위해 나눔

--- day1/src/Star4.java
+++ day1/src/Star4.java
@@ -4,18 +4,16 @@ public class Star4 {
 	public static void main(String[] args) {
 			// 4번 예제
 			
-			/*
 			for(int i=0;i<5;i++) {
 				
 				for(int j=0;j<5;j++) {
 					if(i+j==4 || i==j)
 					System.out.print("["+i+","+j+"]");
 					else
-						System.out.print("     ");
+						System.out.print("     "); // [i,j]는 총 5글자이므로 5칸을 띄움
 				}
 				System.out.println();
 			}
-			*/
 			
 			for(int i=0;i<5;i++) {
 				
@@ -23,7 +21,7 @@ public class Star4 {
 					if(i+j==4 || i==j)
 					System.out.print("*");
 					else
-						System.out.print("     ");
+						System.out.print(" "); // *은 한 글자이므로 한 칸만 띄움
 				}
 				System.out.println();
 			}
---
+++ day1/src/Star8.java
@@ -0,0 +1,107 @@
+
+public class Star8 {
+
+	public static void main(String[] args) {
+		 // 1번 예제
+		
+		for(int i=0; i<5;i++) {
+			System.out.println("*****");
+		}
+		
+		// 행이동 alt+up, down
+	
+		for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		// 2번 예제
+		
+		/*
+		for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				System.out.print("["+i+","+j+"]");
+			}
+			System.out.println();
+		}
+		*/
+		for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				if(i==j)
+				System.out.print("*");
+				else
+					System.out.print("     ");
+			}
+			System.out.println();
+		}
+		
+		// 3번 예제
+		
+		/*
+		for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				if(i+j==4)
+				System.out.print("["+i+","+j+"]");
+				else
+					System.out.print("     ");
+			}
+			System.out.println();
+		}
+		*/
+		
+			for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<5;j++) {
+				if(i+j==4)
+				System.out.print("*");
+				else
+					System.out.print("     ");
+			}
+			System.out.println();
+		}
+			
+			// 4번 예제
+			
+			/*
+			for(int i=0;i<5;i++) {
+				
+				for(int j=0;j<5;j++) {
+					if(i+j==4 || i==j)
+					System.out.print("["+i+","+j+"]");
+					else
+						System.out.print("     ");
+				}
+				System.out.println();
+			}
+			*/
+			
+			for(int i=0;i<5;i++) {
+				
+				for(int j=0;j<5;j++) {
+					if(i+j==4 || i==j)
+					System.out.print("*");
+					else
+						System.out.print("     ");
+				}
+				System.out.println();
+			}
+			
+			for(int i=0;i<5;i++) {
+				
+				for(int j=0;j<5;j++) {
+					if(i+j==4 || i==j)
+					System.out.print("["+i+","+j+"]");
+					else
+						System.out.print("     ");
+				}
+				System.out.println();
+			}
+	}
+
+}
